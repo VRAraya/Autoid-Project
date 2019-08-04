d9f109d226ac4e89659d25d24da0ae192a3ba969
VRAraya <victor.arayar@utem.cl>
2019-08-04 18:23:07 -0400

    Implementacion de advertencia sobre eliminacion de la base de datos

--- autoid-db/setup.js
+++ autoid-db/setup.js
@@ -16,7 +16,7 @@ async function setup () {
     }
   ])
 
-  if(!answer.setup) {
+  if (!answer.setup) {
     return console.log('Nothing happened :)')
   }
 
@@ -37,7 +37,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
