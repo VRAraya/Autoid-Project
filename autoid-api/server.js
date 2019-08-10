b96df45bc9bb5c60bc48a3db2e26a0ee4d4035a9
VRAraya <victor.arayar@utem.cl>
2019-08-09 22:27:51 -0400

    Implementando api

--- autoid-api/server.js
+++ autoid-api/server.js
@@ -13,6 +13,16 @@ const server = http.createServer(app)
 // Middlewares
 app.use('/api', api)
 
+// Express Error Handler Middleware
+app.use((err, req, res, next) => {
+  debug(`Error: ${error.message}`)
+
+  if(err.message.match(/not found/)) {
+    return res.status(404).send({ error: err.message})
+  }
+
+  res.status(500).send({error: err.message})
+})
 server.listen(port, () => {
   console.log(`${chalk.green('[autoid-api]')} server listening on port ${port}`)
 })
