e7a620a567d225d088c6f0340fc3cc1a1e1fd882
VRAraya <victor.arayar@utem.cl>
2020-01-25 02:38:13 -0300

    Tests for API completed

--- autoid-api/server.js
+++ autoid-api/server.js
@@ -23,16 +23,22 @@ app.use((err, req, res, next) => {
   if (err.message.match(/not found/)) {
     return res.status(404).send({ error: err.message })
   }
+  if (err.name === 'UnauthorizedError') {
+    return res.status(401).send({ error: err.message })
+  }
 
   res.status(500).send({ error: err.message })
 })
 
-if (!module.parent) { // Si no estamos requiriendo el server desde otro lado, entonces se ejecuta esto
+if (!module.parent) {
+  // Si no estamos requiriendo el server desde otro lado, entonces se ejecuta esto
   process.on('uncaughtException', handleFatalError)
   process.on('unhandledRejection', handleFatalError)
 
   server.listen(port, () => {
-    console.log(`${chalk.green('[autoid-api]')} server listening on port ${port}`)
+    console.log(
+      `${chalk.green('[autoid-api]')} server listening on port ${port}`
+    )
   })
 }
 
--- autoid-api/tests/api-tests.js
+++ autoid-api/tests/api-tests.js
@@ -8,6 +8,7 @@ const sinon = require('sinon')
 const proxyquire = require('proxyquire')
 
 const agentFixtures = require('./fixtures/agent')
+const metricFixtures = require('./fixtures/metric')
 const serverConfig = require('autoid-config')
 const auth = require('../auth')
 const sign = util.promisify(auth.sign)
@@ -18,6 +19,10 @@ let dbStub = null
 let token = null
 const AgentStub = {}
 const MetricStub = {}
+
+const uuid = 'yyy-yyy-yyy'
+const wrongUuid = 'xxx-yyy-yyy'
+const type = 'air temperature'
 
 const config = serverConfig({
   logging: s => debug(s)
@@ -37,7 +42,34 @@ test.beforeEach(async () => {
   AgentStub.findConnected = sandbox.stub()
   AgentStub.findConnected.returns(Promise.resolve(agentFixtures.connected))
 
-  token = await sign({ admin: true, username: 'root' }, config.auth.secret)
+  AgentStub.findByUuid = sandbox.stub()
+  AgentStub.findByUuid
+    .withArgs(uuid)
+    .returns(Promise.resolve(agentFixtures.byUuid(uuid)))
+  AgentStub.findByUuid.withArgs(wrongUuid).returns(Promise.resolve(null))
+
+  MetricStub.findByAgentUuid = sandbox.stub()
+  MetricStub.findByAgentUuid
+    .withArgs(uuid)
+    .returns(Promise.resolve(metricFixtures.byAgentUuid(uuid)))
+  MetricStub.findByAgentUuid.withArgs(wrongUuid).returns(Promise.resolve(null))
+
+  MetricStub.findByTypeAgentUuid = sandbox.stub()
+  MetricStub.findByTypeAgentUuid
+    .withArgs(type, uuid)
+    .returns(Promise.resolve(metricFixtures.byTypeAgentUuid(type, uuid)))
+  MetricStub.findByTypeAgentUuid
+    .withArgs(type, wrongUuid)
+    .returns(Promise.resolve(null))
+
+  token = await sign(
+    {
+      admin: true,
+      username: 'root',
+      permissions: ['metrics: read']
+    },
+    config.auth.secret
+  )
 
   const api = proxyquire('../api', {
     'autoid-db': dbStub
@@ -67,13 +99,107 @@ test.serial.cb('/api/agents', t => {
     })
 })
 
-test.serial.todo('/api/agents - not authorized')
+test.serial.cb('/api/agents - not authorized', t => {
+  request(server)
+    .get('/api/agents')
+    .expect(401)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) {
+        console.log(`Error in Test ${err.message}`)
+      }
+      t.is(res.statusCode, 401, 'Error must be Unauthorized User')
+      t.end()
+    })
+})
 
-test.serial.todo('/api/agent/:uuid')
-test.serial.todo('/api/agent/:uuid - not found')
+test.serial.cb('/api/agent/:uuid', t => {
+  request(server)
+    .get(`/api/agent/${uuid}`)
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      t.falsy(err, 'should not return an error')
+      const body = JSON.stringify(res.body)
+      const expected = JSON.stringify(agentFixtures.byUuid(uuid))
+      t.deepEqual(body, expected, 'response body should be the expected')
+      t.end()
+    })
+})
 
-test.serial.todo('/api/metrics/:uuid')
-test.serial.todo('/api/metrics/:uuid - not found')
+test.serial.cb('/api/agent/:uuid - not found', t => {
+  request(server)
+    .get(`/api/agent/${wrongUuid}`)
+    .expect(404)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) {
+        console.log(err)
+      }
+      t.truthy(res.body.error, 'should return an error')
+      t.regex(res.body.error, /not found/, 'Error should contains not found')
+      t.end()
+    })
+})
 
-test.serial.todo('/api/metrics/:uuid/:type')
-test.serial.todo('/api/metrics/:uuid/:type - not found')
+test.serial.cb('/api/metrics/:uuid', t => {
+  request(server)
+    .get(`/api/metrics/${uuid}`)
+    .set('Authorization', `Bearer ${token}`)
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      t.falsy(err, 'should not return an error')
+      const body = JSON.stringify(res.body)
+      const expected = JSON.stringify(metricFixtures.byAgentUuid(uuid))
+      t.deepEqual(body, expected, 'response body should be the expected')
+      t.end()
+    })
+})
+
+test.serial.cb('/api/metrics/:uuid - not found', t => {
+  request(server)
+    .get(`/api/metrics/${wrongUuid}`)
+    .set('Authorization', `Bearer ${token}`)
+    .expect(404)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) {
+        console.log(err)
+      }
+      t.truthy(res.body.error, 'should return an error')
+      t.regex(res.body.error, /not found/, 'Error should contains not found')
+      t.end()
+    })
+})
+
+test.serial.cb('/api/metrics/:uuid/:type', t => {
+  request(server)
+    .get(`/api/metrics/${uuid}/${type}`)
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      t.falsy(err, 'should not return an error')
+      const body = JSON.stringify(res.body)
+      const expected = JSON.stringify(
+        metricFixtures.byTypeAgentUuid(type, uuid)
+      )
+      t.deepEqual(body, expected, 'response body should be the expected')
+      t.end()
+    })
+})
+
+test.serial.cb('/api/metrics/:uuid/:type - not found', t => {
+  request(server)
+    .get(`/api/metrics/${wrongUuid}/${type}`)
+    .expect(404)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) {
+        console.log(err)
+      }
+      t.truthy(res.body.error, 'should return an error')
+      t.regex(res.body.error, /not found/, 'Error should contains not found')
+      t.end()
+    })
+})
